0fa1d545a970d5c8a1cad84c94c14a17a2c6c8a7
Patrick Huck <phuck@lbl.gov>
2020-04-01 21:53:31 -0700

    tag.sh: commit, tag and push

--- tag.sh
+++ tag.sh
@@ -2,11 +2,16 @@
 
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 TAG"
-  echo "Example: $0 3.8.2-slim-1.0"
+  echo "Example: $0 3.8.2-slim-v1.0"
   exit 1
 fi
 
 tag=$1
-git tag $tag
 url=https://github.com/tschaume/shrink-linalg/blob/$tag/Dockerfile
 echo "- [$tag]($url): " `cat requirements.txt | tr '\n' ' '` >> README.md
+
+git add README.md
+git commit -m "$tag"
+git tag $tag
+git push
+git push --tags
